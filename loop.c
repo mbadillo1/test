79a76a71d9bf671f0e2a5950195e3ce00776e706
mbadillo1 <mbadillo1@csc208.csudh.edu>
2020-02-26 16:38:57 -0800

    loop

---
+++ loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(void)
+{
+	double x2;
+	int x=0,i=0 ;
+	for(i=0; i <10; i++)
+	{
+	 
+
+	 x++;
+ 	 x++;	 
+	}
+
+	x2=x/20;
+	printf(" %d  divided by 20  is %.3d \n",x,(double) x2);
+	printf("the final of value of  x is  %d \n ",x);
+}
